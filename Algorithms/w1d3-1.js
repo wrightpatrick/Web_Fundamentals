e8d52b2c102fc5ab3beba4d980ca3191ec840abd
Patrick Wright <patrickdane@me.com>
2021-12-15 11:26:11 -0500

    algos

---
+++ Algorithms/w1d3-1.js
@@ -0,0 +1,6 @@
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+
+
+
+console.log("there are " + countPositives + " positive values in the array!");
---
+++ Algorithms/w1d3wrong.js
@@ -0,0 +1,12 @@
+//problem 3
+var countPositives =0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+
+for(var i=0, [i]<=Array.length, i++){
+    if (i > 0){
+        countpositive=+
+    }
+}
+
+console.log("there are " + countPositives + " positive values in the array!");
+
