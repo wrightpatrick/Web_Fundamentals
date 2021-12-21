5b1bbc50c11103c18f5e5575ad5b8e2d8b7b4282
Patrick Wright <patrickdane@me.com>
2021-12-21 14:47:07 -0500

    loop challenges and algos

---
+++ JS/loop_challenges/loop_challenges.js
@@ -0,0 +1,45 @@
+// #1
+
+// for(i=0; i<=20; i++){
+//     if(i % 2 != 0){
+//     console.log(i);
+//     }
+// }
+
+// #2
+
+// for(i=100; i>=0; i--){
+//     if(i % 3==0){
+//         console.log(i);
+//     }
+// }
+
+// #3
+
+// var nums =[4, 2.5, 1, -0.5, -2, -3.5];
+// i=0;
+// while(i<nums.length){
+//     console.log(nums[i]);
+//     i++;
+// }
+
+// #4
+
+// var sum=0;
+// var i=0;
+// while(i<=100){
+//     sum = sum + i;
+//     i++;
+// }
+// console.log(sum);
+
+// #5
+
+var product=1;
+var i=1;
+while(i<=12){
+    product *= i;
+    i++;
+}
+
+console.log(product);
