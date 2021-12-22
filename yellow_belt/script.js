6e11b091a62e9c457a113ef2a05445a8a7f6723e
Patrick Wright <patrickdane@me.com>
2021-12-22 18:08:36 -0500

    yellow belt

---
+++ yellow_belt/script.js
@@ -0,0 +1,19 @@
+var element = document.getElementById("liveImage");
+
+
+function changePic(element){
+    (liveImage.src = "img/succulents-2.jpg");
+    
+}
+
+function changePicBack(element){
+    (liveImage.src = "img/succulents-1.jpg");
+    
+}
+var cookies = docujment.getElementById("cookiePolicy");
+
+function remove(cookies){
+     cookies = hidden;
+}
+    
+
